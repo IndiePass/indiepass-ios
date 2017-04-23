5003d1b43839f547e4638a393409590a91c01ff5
Edward Hinkle <eddie@meetmaestro.com>
2017-04-23 00:58:09 -0400

    Add some initial legwork for indigenous

---
+++ Indigenous/mf2-parser.swift
@@ -0,0 +1,13 @@
+//
+//  mf2-parser.swift
+//  Indigenous
+//
+//  Created by Eddie Hinkle on 4/22/17.
+//  Copyright © 2017 Studio H, LLC. All rights reserved.
+//
+
+import Foundation
+
+//class mf2-parser {
+//    
+//}
